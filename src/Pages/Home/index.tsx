38d51f54f4c1db69098d0bde67544ec5521c5025
lucasfmarques2 <lucasfreitasm3@hotmail.com>
2025-01-14 17:34:02 -0300

    Alinhando header home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,19 @@
+import coffeheader from '../../assets/headerImg.svg'
+import { ShoppingCart } from 'phosphor-react'
+import { HomeContainer } from './style'
+
 export function Home(){
-    return <h1>To na home</h1>
+    return (
+        <HomeContainer>
+            <h1>Encontre o café perfeito para qualquer hora do diaEncontre o café perfeito para qualquer hora do dia</h1>
+            <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+            <p><ShoppingCart size={20} /> Compra simples e segura</p>
+            <p><ShoppingCart size={20} /> Compra simples e segura</p>
+            <p><ShoppingCart size={20} /> Compra simples e segura</p>
+            <p><ShoppingCart size={20} /> Compra simples e segura</p>
+
+            <img src={coffeheader} alt="Imagem de um copo de café" />
+
+        </HomeContainer>
+    )
 }
---
+++ src/Pages/Home/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+    padding: 0 16rem ;
+    margin-bottom: 2rem;
+`
+
+export const homeHeader = styled.div`
+   font-size: 'Baloo 2';
+`
